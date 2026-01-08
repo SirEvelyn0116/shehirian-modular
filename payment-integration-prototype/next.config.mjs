1f27cfa5246e7fd72867fa59cef4fb33f675a0d5
Laurence Evelyn <adrian_laurence@hotmail.com>
2026-01-08 05:04:35 -0500

    Disable missing suspense bailout for deployment

--- payment-integration-prototype/next.config.mjs
+++ payment-integration-prototype/next.config.mjs
@@ -1,4 +1,9 @@
 ﻿/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Disable static generation for pages that use searchParams
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
